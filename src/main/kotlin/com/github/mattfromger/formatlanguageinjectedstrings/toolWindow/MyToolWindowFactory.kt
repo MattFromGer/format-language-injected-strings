5f89e085e484effc8ac2bef9704c72b8541b30bd
GitHub Action <action@github.com>
2025-03-04 09:59:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/mattfromger/formatlanguageinjectedstrings/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/mattfromger/formatlanguageinjectedstrings/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.mattfromger.formatlanguageinjectedstrings.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.mattfromger.formatlanguageinjectedstrings.MyBundle
+import com.github.mattfromger.formatlanguageinjectedstrings.services.MyProjectService
 import javax.swing.JButton
 
 
